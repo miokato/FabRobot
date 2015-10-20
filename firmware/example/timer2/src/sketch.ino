246e06d1b4fa59c6128aa1e648b8f5d3067303a2
mio_k <miokato@me.com>
2015-10-20 09:56:05 +0900

    add timer2

---
+++ firmware/example/timer2/src/sketch.ino
@@ -0,0 +1,26 @@
+#include <MsTimer2.h>
+#define LED_PIN 13
+#define SENSOR_PIN 0
+
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  MsTimer2::set(100, ReadSensor);
+  MsTimer2::start();
+}
+
+void loop()
+{
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
+}
+
+void ReadSensor() {
+  int raw = analogRead(SENSOR_PIN);
+  Serial.println(raw);
+}
+
